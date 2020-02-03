dd24f7123031420fecc02e748896dbaa5d7bcad1
Faris-M <mouwaffakfaris@gmail.com>
2020-02-03 20:18:37 +0100

    params

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,3 +1,5 @@
+require 'gossip'
+
 class ApplicationController < Sinatra::Base
   get '/' do #première route
     erb :index
@@ -6,4 +8,11 @@ class ApplicationController < Sinatra::Base
   get '/gossips/new/' do # deuxieme route vers fichier gossip
       erb :new_gossip
   end
+
+  post '/gossips/new/' do
+    Gossip.new(params["gossip_author"], params["gossip_content"]).save
+  end
+ 
+
+
 end
