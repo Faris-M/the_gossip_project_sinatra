0d872a2378338015b56b912b9fdf9f1aff07a047
Faris-M <mouwaffakfaris@gmail.com>
2020-02-03 15:58:42 +0100

    commit

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,6 +1,9 @@
-
 class ApplicationController < Sinatra::Base
-  get '/' do
+  get '/' do #première route
     erb :index
+  end
+
+  get '/gossips/new/' do # deuxieme route vers fichier gossip
+      erb :new_gossip
   end
 end
